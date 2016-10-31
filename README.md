8513890896d6382a19114b5011136ee6d74983c2
David Lindkvist <david@14islands.com>
2016-10-31 17:50:20 +0100

    Initial commit of es6/stylus/pug framework

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # handbook
 The 14islands handbook.
+
+## install
+
+`npm i`
+
+## run development watcher
+
+`npm start`
+
+## build for production
+
+`npm run build`
+
+# TODO
+* Add jQuery as vendor script
+* Add component-loader-js
+* Build awesome stuff!
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import webfontloader from 'webfontloader'
+
+console.log('hello javascript')
+
+// Wait for webfont to finish loading custom font
+// webfontloader.load({
+//   custom: {
+//     families: ['My Custom Font']
+//   },
+//   active: () => {
+//     console.log('Custom font is now loaded')
+//   }
+// })
